7d8405ce0848c8b4dbf1648de30fca5d5d05ff38
Roman <82847419+Roman-oze@users.noreply.github.com>
2024-05-10 18:57:56 +0600

    added layout folder

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('layout._head')
+</head>
+<body>
+
+    {{-- <nav class="navbar navbar-expand-lg navbar-light  fixed-top facilty p-2 t-center"> --}}
+    <nav class="navbar navbar-expand-lg navbar-light facilty fixed-top p-2 ">
+
+        @include('layout._nav')
+      </nav>
+
+      <header>
+
+            <div class="container-fluid mt-4">
+                <br>
+                <br>
+                <br>
+                 @yield('headerpage')
+             </div>
+      </header>
+
+
+<div class="container-fluid">
+    @yield('content')
+</div>
+
+    <footer>
+        @include('layout._footer')
+    </footer>
+
+    <script>
+        @include('layout._script')
+    </script>
+
+</body>
+</html>
