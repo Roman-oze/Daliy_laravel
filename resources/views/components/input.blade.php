e00eb47ba4b0b61e1a2436353ea9ad8f8a65c6af
Roman <82847419+Roman-oze@users.noreply.github.com>
2024-05-27 21:20:50 +0600

    delete

--- resources/views/components/input.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="form-group ">
-    <label>{{$label}}</label>
-    <input type="{{$type}}" class="form-control" name="{{$name}}" placeholder="">
-   <span class="text-danger">
-      @error('name')
-      {{$message}}
-      @enderror
-   </span>
-  </div>
